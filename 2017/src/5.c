8404e73bcce887996567ffb13fad21b2f8fb86a8
Alexander Fleischer <fleisch91@gmail.com>
2018-01-17 09:42:49 +0100

    Finished day 5 part 1 and 2

--- 2017/src/5.c
+++ 2017/src/5.c
@@ -5,8 +5,15 @@
 void get_column_from_file(FILE *, int [], int);
 int jump(int [], int, int);
 void perform_jumps(int [], int);
+void display_controls(int, int);
 
 int main(int argc, const char *argv[]) {
+
+    if (argc != 3) {
+        printf("Error: Requires 2 arguments—data file and number of lines.\n\n");
+        abort();
+    }
+
     char *mode = "r";
     char filename[25];
     strcpy(filename, argv[1]);
@@ -37,18 +44,39 @@ void get_column_from_file(FILE *ifp, int array[], int number_of_lines) {
 
 void perform_jumps(int jump_offsets[], int number_of_lines) {
     int current_position = 0;
+    int number_of_jumps = 0;
+
     printf("Current position is %d with value %d.\n", current_position, jump_offsets[current_position]);
+
     while (current_position >= 0 && current_position < number_of_lines) {
         current_position = jump(jump_offsets, current_position, number_of_lines);
+        number_of_jumps++;
     }
+    printf("\nPerformed %d jumps to escape the maze!\n", number_of_jumps);
 }
 
 int jump(int jump_offsets[], int current_position, int number_of_lines) {
     int new_position = 0;
+
     new_position = current_position + jump_offsets[current_position];
-    jump_offsets[current_position] += 1;
-    printf("Press any button to jump.\n");
-    getchar();
-    printf("Jumped to position %d\n\n", new_position);
+
+    if (jump_offsets[current_position] >= 3) 
+        jump_offsets[current_position]--;
+    else
+        jump_offsets[current_position]++;
+
+    int is_controls = 0;
+   
+    display_controls(is_controls, new_position); 
+
     return new_position;
+}
+
+void display_controls(int is_controls, int new_position) {
+    if (is_controls) {
+        printf("Press any button to jump.\n");
+        getchar();
+        printf("Jumped to position %d\n\n", new_position);
+    }
+    return;
 }
